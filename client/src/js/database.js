43c8e004b0e321cf7e262bc1a42058009512b130
PeachDoors <taylorpterrill@gmail.com>
2022-11-03 19:20:57 -0600

    add comments

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,7 +12,7 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// Logic for editing the database
 export const putDb = async (content) => {
   console.log('PUT request');
   const jateDb = await openDB('jate', 1);
@@ -26,7 +26,7 @@ export const putDb = async (content) => {
   console.log('🚀 - data saved to the database', result);
 };
 
-// TODO: Add logic for a method that gets all the content from the database
+// Logic to get information from the database
 export const getDb = async () => {
   console.log('GET request');
   const jateDb = await openDB('jate', 1);
